743cadcb0df53691432313fc60a7d40d590de770
Iliyan Peychev <iliyan.peychev@liferay.com>
2014-11-26 18:07:50 +0100

    Add tests initial load (WIP)

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,13 @@ var del = require('del');
 var exec = require('child_process').exec;
 var fs = require('fs');
 var gulp = require('gulp');
+var esformatter = require('esformatter');
+var istanbul = require('gulp-istanbul');
+var merge = require('merge-stream');
 var rename = require('gulp-rename');
 var runSequence = require('run-sequence');
 var template = require('gulp-template');
 var uglify = require('gulp-uglify');
-var watch = require('gulp-watch');
 
 gulp.task('build', function(callback) {
     runSequence('clean', ['config', 'loader-min', 'source-min', 'modules', 'build-config', 'vendor', 'demo'], callback);
@@ -52,6 +54,18 @@ gulp.task('demo', function() {
         .pipe(gulp.dest('dist'));
 });
 
+gulp.task('format', function() {
+    var src = gulp.src(['src/**/*.js'])
+        .pipe(esformatter())
+        .pipe(gulp.dest('src'));
+
+    var test = gulp.src(['test/**/*.js'])
+        .pipe(esformatter())
+        .pipe(gulp.dest('test'));
+
+    return merge(src, test);
+});
+
 gulp.task('js', ['combine-js', 'vendor-js'], function() {
     return gulp.src('src/template/loader.template')
         .pipe(template({
@@ -74,6 +88,32 @@ gulp.task('modules', function() {
         .pipe(gulp.dest('dist/modules'));
 });
 
+gulp.task('lint', function() {
+  return gulp.src(['src/**/*.js', 'test/**/*.js'])
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter(require('jshint-stylish')));
+});
+
+gulp.task('test', function() {
+  return gulp.src(['test/**/*.js', '!test/fixture/*.js'])
+    .pipe(plugins.mocha());
+});
+
+gulp.task('test-cover', function() {
+  return gulp.src(['src/**/*.js'])
+    .pipe(plugins.istanbul());
+});
+
+gulp.task('test-coverage', ['test-cover'], function() {
+  return gulp.src(['test/**/*.js', '!test/fixture/*.js'])
+    .pipe(plugins.mocha())
+    .pipe(plugins.istanbul.writeReports());
+});
+
+gulp.task('test-watch', function() {
+  gulp.watch('src/**/*.js', ['test']);
+});
+
 gulp.task('source-min', ['js'], function() {
     return gulp.src('dist/js/source.js')
         .pipe(uglify())
@@ -94,8 +134,6 @@ gulp.task('vendor-js', function() {
     .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('watch', ['build'], function () {
-    watch('src/**/*.*', function (files, cb) {
-        gulp.start('build', cb);
-    });
+gulp.task('watch', ['build'], function() {
+    gulp.watch('src/**/*', ['build']);
 });
